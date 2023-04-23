c6f9d43550819b224e21e3967a9777709cc0db05
Michał Guz <michal.guz.85@gmail.com>
2023-04-23 20:15:00 +0200

    Fetching championships

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [champs, setChamps] = useState([])
+
+  useEffect(() => {
+    fetchChamps()
+  }, [])
+
+  const fetchChamps = () => {
+    fetch("http://localhost:8080/championships")
+      .then((response) => response.json())
+      .then((data) => setChamps(data));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Frisbee championships</h2>
+        {champs.map((champ, index) => (
+          <div key={index}>
+            {`${champ.name} - ${champ.city} - ${champ.date}`}
+          </div>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
