6cf8530e973195490a380c051e1905fea566051b
Michał Guz <michal.guz.85@gmail.com>
2023-07-14 09:55:12 +0200

    Fetching player details

--- src/components/PlayerDetailsComponent.js
+++ src/components/PlayerDetailsComponent.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 const PlayerDetailsComponent = () => {
-
+    const [player, setPlayer] = useState({});
     const params = useParams();
+    useEffect(() => {
+        fetchPlayer(params.playerId);
+    }, []);
 
-    return <h2>Player details: {params.playerId}</h2>
+const fetchPlayer = (playerId) => {
+    fetch(`http://localhost:8080/players/${playerId}`)
+    .then((response) => response.json())
+    .then((data) => setPlayer(data));
+};
+
+    
+
+    return <h2>{`Player details: ${player.name}, shirt number: ${player.shirtNumber}`}</h2>
 }
 
 export default PlayerDetailsComponent;
