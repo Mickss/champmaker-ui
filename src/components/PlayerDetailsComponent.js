b9c365836fed8023cc6cca2fe67c50293ae6759e
Michał Guz <michal.guz.85@gmail.com>
2023-07-14 09:57:35 +0200

    Fix formatting

--- src/components/PlayerDetailsComponent.js
+++ src/components/PlayerDetailsComponent.js
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 const PlayerDetailsComponent = () => {
-    const [player, setPlayer] = useState({});
-    const params = useParams();
-    useEffect(() => {
-        fetchPlayer(params.playerId);
-    }, []);
+  const [player, setPlayer] = useState({});
+  const params = useParams();
+  useEffect(() => {
+    fetchPlayer(params.playerId);
+  }, []);
 
-const fetchPlayer = (playerId) => {
+  const fetchPlayer = (playerId) => {
     fetch(`http://localhost:8080/players/${playerId}`)
-    .then((response) => response.json())
-    .then((data) => setPlayer(data));
+      .then((response) => response.json())
+      .then((data) => setPlayer(data));
+  };
+
+  return (
+    <h2>{`Player details: ${player.name}, shirt number: ${player.shirtNumber}`}</h2>
+  );
 };
-
-    
-
-    return <h2>{`Player details: ${player.name}, shirt number: ${player.shirtNumber}`}</h2>
-}
 
 export default PlayerDetailsComponent;
