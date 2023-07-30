afd93294f88898081247cb7b471e6793b28b5bce
Michał Guz <michal.guz.85@gmail.com>
2023-07-30 20:09:38 +0200

    Displaying the team registered for the tournament

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import ChampionshipsListComponent from './components/ChampionshipsListComponent';
-import PlayersListComponent from './components/PlayersListComponent';
-import PlayerDetailsComponent from './components/PlayerDetailsComponent';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import RootLayout from './RootLayout';
-import TeamsListComponent from './components/TeamsListComponent';
-import TeamDetailsComponent from './components/TeamDetailsComponent';
-import ChampionshipDetailsComponent from './components/ChampionshipDetailsComponent';
+import ChampionshipsListComponent from "./components/ChampionshipsListComponent";
+import PlayersListComponent from "./components/PlayersListComponent";
+import PlayerDetailsComponent from "./components/PlayerDetailsComponent";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./RootLayout";
+import TeamsListComponent from "./components/TeamsListComponent";
+import TeamDetailsComponent from "./components/TeamDetailsComponent";
+import ChampionshipDetailsComponent from "./components/ChampionshipDetailsComponent";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
       children: [
-        {path: "/", element: <ChampionshipsListComponent />},
-        {path: '/:champId', element: <ChampionshipDetailsComponent />},
-        {path: "players", element: <PlayersListComponent />},
-        {path: 'players/:playerId', element: <PlayerDetailsComponent /> },
-        {path: "teams", element: <TeamsListComponent />},
-        {path: 'teams/:teamId', element: <TeamDetailsComponent />}
-      ]
-    }
+        { path: "/", element: <ChampionshipsListComponent /> },
+        { path: "/:champId", element: <ChampionshipDetailsComponent /> },
+        { path: "players", element: <PlayersListComponent /> },
+        { path: "players/:playerId", element: <PlayerDetailsComponent /> },
+        { path: "teams", element: <TeamsListComponent /> },
+        { path: "teams/:teamId", element: <TeamDetailsComponent /> },
+      ],
+    },
   ]);
 
   return <RouterProvider router={router} />;
